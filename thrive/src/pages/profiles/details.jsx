1df5cf2d806b6211be9f37673020ad93d3cc47cd
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 07:06:30 -0400

    mock profile example

--- thrive/src/pages/profiles/details.jsx
+++ thrive/src/pages/profiles/details.jsx
@@ -12,7 +12,10 @@ export class ProfileDetailsPage extends React.Component {
   render() {
     return (
       <>
-        <center><h2>Jimmy Bob</h2></center>
+        <center><h2><strong>Jimmy Bob</strong></h2></center>
+        <center><img src="https://pbs.twimg.com/profile_images/786711098955358208/T0wKRr4I_400x400.jpg"/></center>
+        <center><h4>Company Name</h4></center>
+        <center><h5>Job Title</h5></center>
       </>
     )
   }
