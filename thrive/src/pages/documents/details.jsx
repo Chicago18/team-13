a7af13bf238741824957cc38d3073d07f78c6cc1
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2018-10-13 05:37:55 -0500

    Broken document details using mobx decorators

--- thrive/src/pages/documents/details.jsx
+++ thrive/src/pages/documents/details.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
-export class DocumentDetailsPage extends React.Component {
+export class DocumentRepo {
+
+	constructor(baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	get(id) {
+		return fetch(this.baseUrl + '/documents/' + id).then(x => x.json()); 
+	}
+}
+
+export @observer
+class DocumentDetailsPage extends React.Component {
+
 
   id = null;
+  @observable document = null;  
 
   constructor(props) {
     super(props);
     this.id = this.props.match.params.id;
+    this.repo = new DocumentRepo('http://52.201.219.91:8080')
+  }
+
+  async componentWillMount() {
+  	this.document = await this.repo.get(this.id);
   }
 
   render() {
+  	if (!this.document) {
+  		return 'Loading...';
+  	}
+
     return (
     	<div>
     	<div class="row">
     		<div class="col-sm-5">
-    			<h2>Stakeholder Engagement in Today’s Polarized Environment: Knowing How to Respond</h2>
+    			{this.document.title}
+    			
+    			
 			</div>
     		<div class="col-sm-5">
 
-<div class="card d-flex flex-row">
-  <img src="https://dummyimage.com/96/000/fff" class="img-responsive img-rounded"/>
-  <div class="card-body">
-    <h3 class="card-title">Apoorva Gandhi</h3>
-    <p class="card-text">VP of Multicultural Affairs,  Marriott International</p>
-  </div>
-</div>
+			<div class="card d-flex flex-row">
+			  <img src="https://dummyimage.com/96/000/fff" class="img-responsive img-rounded"/>
+			  <div class="card-body">
+			    <h3 class="card-title">Apoorva Gandhi</h3>
+			    <p class="card-text">VP of Multicultural Affairs,  Marriott International</p>
+			  </div>
+			</div>
   
 			</div>
 			<div class="col-sm-2">
