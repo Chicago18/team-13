f04ecbde8251c1a9e89aadf1eacd1e012c57941c
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 07:16:19 -0400

    register added

---
+++ thrive/src/pages/register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export class RegisterPage extends React.Component {
+
+  render() {
+    return (
+      <>
+        <p>REGISTER</p>
+      </>
+    );
+  }
+}
