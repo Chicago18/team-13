31ba21b75b73a549f03562c2876aafe5cb9fde32
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 07:35:27 -0400

    form added to register

--- thrive/src/pages/register.jsx
+++ thrive/src/pages/register.jsx
@@ -7,16 +7,20 @@ export class RegisterPage extends React.Component {
     return (
       <>
       <form>
-        <div class="form-group">
+      <div class="form-group">
+        <label for="Enter Your Name">First Name</label>
+        <input type="name" class="form-control" id="John" aria-describedby="emailHelp" placeholder="John"/>
+      </div>
+      <div class="form-group">
+        <label for="Enter your Last Name">Last Name</label>
+        <input type="name" class="form-control" id="Doe" aria-describedby="emailHelp" placeholder="Doe"/>
+      </div>
+      <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
+      </div>
         <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
