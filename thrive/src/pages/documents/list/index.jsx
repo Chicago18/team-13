7657ba669437f0d4793aeb8cecced6400fe99935
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 09:01:49 -0400

    cards reworked

--- thrive/src/pages/documents/list/index.jsx
+++ thrive/src/pages/documents/list/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { DocumentCard } from '../../../components/document-card'
 
 import './index.css'
 
 export class DocumentListPage extends React.Component {
 
   render() {
+
     return (
       <>
         <p>DOCUMENTS</p>
         <Link to="/documents/1">#1</Link>
-
 
         <div class="card" style={{width: '100%'}}>
   <div class="card-body">
@@ -18,20 +19,11 @@ export class DocumentListPage extends React.Component {
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
-
-        <div class="card" style={{width: '100%'}}>
-  <div class="card-body">
-    <h5 class="card-title">Card 2</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
-  </div>
-</div>
+      <DocumentCard title="Card 999" subtitle="Card subtitle" description="foobar" />
 
 
         <div class="card" style={{width: '100%'}}>
@@ -40,7 +32,7 @@ export class DocumentListPage extends React.Component {
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
@@ -50,7 +42,7 @@ export class DocumentListPage extends React.Component {
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
@@ -60,7 +52,7 @@ export class DocumentListPage extends React.Component {
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
@@ -70,7 +62,7 @@ export class DocumentListPage extends React.Component {
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
@@ -80,11 +72,11 @@ export class DocumentListPage extends React.Component {
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a> 
+    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
-        
+
       </>
     );
   }
