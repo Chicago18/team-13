13748fb2c24ace960ec66ee61b047b0e134a8c32
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 06:54:45 -0400

    profile added to navigation

--- thrive/src/routes.js
+++ thrive/src/routes.js
@@ -1,6 +1,8 @@
 import { HomePage } from './pages/home'
 import { DocumentDetailsPage } from './pages/documents/details';
 import { DocumentListPage } from './pages/documents/list';
+import { ProfileDetailsPage } from './pages/profiles/details';
+import { ProfileListPage } from './pages/profiles/list';
 
 const TODO = () => 'TODO!!!'
 
@@ -27,6 +29,25 @@ export const routes = [
         path: "/documents/:id",
         text: "Documents",
         main: DocumentDetailsPage
+      },
+    ]
+  },
+  {
+    path: "/profiles",
+    text: "Profiles",
+    icon: "home",
+    exact: true,
+    main: ProfileListPage,
+    routes: [
+      {
+        path: "/profiles/create",
+        text: "Profile",
+        main: TODO,
+      },
+      {
+        path: "/profiles/:id",
+        text: "Profile",
+        main: ProfileDetailsPage
       },
     ]
   },
