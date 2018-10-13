4b49d200d1d5f3450c9e46a28bfffc1dea398750
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 07:12:20 -0400

    html updates

--- thrive/src/pages/home.jsx
+++ thrive/src/pages/home.jsx
@@ -4,8 +4,8 @@ export class HomePage extends React.Component {
   render() {
     return (
       <div className="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p className="lead">Use this document as a way to quickly start any new project.<br /> All you get is this text and a mostly barebones HTML document.</p>
+        <h1>Welcome to Out and Equal</h1>
+        <p className="lead">This site is for you to: .<br /> :).</p>
       </div>);
   }
 }
