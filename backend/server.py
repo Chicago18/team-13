da25c6b94c1b4ff483809be8565809a9d166436b
nicolavic98 <nicolavic98@gmail.com>
2018-10-13 00:54:56 -0400

    Major updates

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,22 @@
-from flask import Flask
+import psycopg2
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+conn = psycopg2.connect("dbname='unify' user='postgres' host='52.201.219.91' password='foobar'")
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/users')
+def get_users():
+    cur = conn.cursor()
+    cur.execute("""SELECT * from person""")
+    rows = cur.fetchall()
+    return jsonify(rows)
+
+
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8080)
+      app.run(host='127.0.0.1', port=8080)
